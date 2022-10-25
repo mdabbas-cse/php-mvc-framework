0f64e03a51f7d50ef0a27e5851a3fc90ca7e94f1
mdabbas-cse <gmabbas44@gmail.com>
2022-10-25 23:18:59 +0600

    Create 404.php

---
+++ resources/views/404.php
@@ -0,0 +1,7 @@
+<?php $this->setSiteTile('404'); ?>
+<?php $this->start('body'); ?>
+<div class="text-center mt-4">
+  <h1 class="text-danger">404</h1>
+  <h3>Not Found!</h3>
+</div>
+<?php $this->end(); ?>
