066c7d2dea9b4c347089af56a9d86cf43fad93e2
mdabbas-cse <gmabbas44@gmail.com>
2022-11-04 01:07:14 +0600

    create: params routing

--- framework/Router.php
+++ framework/Router.php
@@ -19,14 +19,74 @@ class Router
     return $routes;
   }
 
+  /**
+   * @method for getCallback
+   * @param $uri
+   * @param $controller
+   */
+  private function getCallback($url, $method)
+  {
+    $url = trim($url, '/');
+    $routes  = $this->routes[$method] ?? [];
+
+    $routeParams = false;
+
+    foreach ($routes as $route => $callback) {
+      echo '<pre>';
+      echo 'route: ' . $route . '<br>';
+      var_dump($callback);
+      // echo '</pre>';
+
+      $route = trim($route, '/');
+      $routeName = [];
+
+      if (!$route)  continue;
+      // find all route name from route name
+      if (preg_match_all('/\{(\w+)(:[^}]+)?}/', $route, $matches)) {
+        $routeName = $matches[1];
+      }
+      // echo '<pre>';
+      echo 'routeName: ';
+      var_dump($routeName);
+
+
+      // replace all route name with regex
+      $routeRegex = "@^" . preg_replace_callback(
+        '/\{\w+(:([^}]+))?}/',
+        function ($m) {
+          return isset($m[2]) ? "({$m[2]})" : '(\w+)';
+        },
+        $route
+      ) . "$@"; // end of regex
+
+
+      // var_dump($routeName);
+      // var_dump($url);
+      var_dump($routeRegex);
+      echo '</pre>';
+      echo '<hr>';
+      // test and match current route with route regex
+      if (preg_match_all($routeRegex, $url, $matchesValue)) {
+        echo '<pre>';
+        var_dump($matchesValue);
+        echo '</pre>';
+        // array_shift($matches);
+        // $routeParams = array_combine($routeName, $matches);
+        // break;
+      }
+    }
+  }
+
   public function callRouter($url, $requestType)
   {
     $request = new Request();
     $response = new Response();
+    $callback = $this->routes[$requestType][$url] ?? false;
+    if (!$callback) {
+      $callback = $this->getCallback($url, $requestType);
+      // dd($callback);
+    }
     if (array_key_exists($url, $this->routes[$requestType])) {
-
-      $callback = $this->routes[$requestType][$url];
-
       // check if route is a callback function render closure
       if (is_object($callback)) {
         return call_user_func($callback);
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use MVC\App\Http\Controllers\UserController;
 
 // $routes->get('/', [HomeController::class, 'index']); // for home page
 $routes->get('/', [AuthController::class, 'loginForm']); // for home page
-$routes->get('user', function () {
-  echo 'user';
-}); // callback function
+// $routes->get('user', function () {
+//   echo 'user';
+// }); // callback function
 $routes->get('contact', 'contact'); // for access view without any call back function
 $routes->post('contact', [UserController::class, 'store']); // for post request
 
@@ -16,8 +16,7 @@ $routes->post('auth-register', [AuthController::class, 'register']);
 $routes->get('auth-login', [AuthController::class, 'loginForm'])->name('auth.loginFrom');
 $routes->post('auth-login', [AuthController::class, 'login'])->name('login');
 
-$routes->get('users/([0-9]+)/show', [UserController::class, 'show']);
-$routes->get('users/([0-9]+)/edit', 'app/Http/Controllers/edit-user.php');
-$routes->get('users/([0-9]+)/delete', 'app/Http/Controllers/delete-user.php');
-$routes->get('users/([0-9]+)/posts', 'app/Http/Controllers/user-posts.php');
+$routes->get('users/{id:\d+}/{user}', [UserController::class, 'show']);
+$routes->get('users/{id:\d+}/{profile}', 'app/Http/Controllers/edit-user.php');
+
 // dd($routes);
