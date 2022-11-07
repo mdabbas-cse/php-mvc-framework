f4b9094f8d7d6df57cc295af91dd972419c3d9d1
mdabbas-cse <gmabbas44@gmail.com>
2022-11-08 01:17:20 +0600

    new Module Csrf, Str, and Date

---
+++ framework/Facades/Csrf.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace zFramework\Core;
+
+use Lora\Core\Framework\Facades\Str;
+use Lora\Core\Framework\Request;
+
+class Csrf
+{
+  /**
+   * Csrf will change timeout is when finish
+   */
+  static $timeOut = (10 * 60);
+
+  /**
+   * Show csrf input
+   */
+  public static function csrf()
+  {
+    echo "<input type='hidden' name='_token' value='" . self::get() . "' />";
+  }
+
+  /**
+   * Get Csrf Token
+   * @return string
+   */
+  public static function get(): string
+  {
+    if ((!@$_SESSION['csrf_token'] || time() > @$_SESSION['csrf_token_timeout'])) self::set();
+    return $_SESSION['csrf_token'];
+  }
+
+  /**
+   * Set Csrf Token randomly
+   * @return void
+   */
+  public static function set()
+  {
+    $_SESSION['csrf_token_timeout'] = time() + self::$timeOut;
+    $_SESSION['csrf_token'] = Str::rand(30);
+  }
+
+  /**
+   * Unset Csrf Token
+   */
+  public static function unset()
+  {
+    unset($_SESSION['csrf_token']);
+  }
+
+  /**
+   * Get remain time for timeout
+   * @return int
+   */
+  public static function remainTimeOut(): int
+  {
+    return @$_SESSION['csrf_token_timeout'] - time();
+  }
+
+  /**
+   * Check is a valid Csrf Token
+   * $alwaysTrue parameter: if you wanna do not check it you can use $alwaysTrue = true
+   * @param bool $alwaysTrue
+   * @return bool
+   */
+  public static function check($alwaysTrue = false, Request $request): bool
+  {
+    if (($request->isGet() && $request->input('_token') != self::get()) && $alwaysTrue != true) return false;
+    return true;
+  }
+}
---
+++ framework/Helpers/Date.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Lora\Core\Framework\Helpers;
+
+class Date
+{
+  /**
+   * Set Timezone
+   * @param string $set
+   * @return int
+   */
+  public static function setLocale(string $set): int
+  {
+    return date_default_timezone_set($set);
+  }
+
+  /**
+   * Get Current Timezone
+   * @return string
+   */
+  public static function locale(): string
+  {
+    return date_default_timezone_get();
+  }
+
+  /**
+   * Date reformat
+   * @param string|int $date
+   * @param string $format
+   * @return string 
+   */
+  public static function format(string $date, string $format = 'd.m.Y'): string
+  {
+    return date($format, (is_string($date) ? strtotime($date) : $date));
+  }
+
+  /**
+   * Get Now With Date
+   * @param string $format
+   * @return string
+   */
+  public static function now(string $format = 'd.m.Y H:i'): string
+  {
+    return date($format);
+  }
+
+  /**
+   * Current Timestamp For Mysql Or Mssql
+   * @return string
+   */
+  public static function timestamp(): string
+  {
+    return date('Y-m-d H:i:s');
+  }
+
+  /**
+   * TimeAgo
+   * @param string $date
+   * @return string
+   */
+  public static function timeAgo($date): string
+  {
+    $time = time() - strtotime($date); // to get the time since that moment
+    $time = ($time < 1) ? 1 : $time;
+    $tokens = [
+      31536000 => 'year',
+      2592000 => 'month',
+      604800 => 'week',
+      86400 => 'day',
+      3600 => 'hour',
+      60 => 'minute',
+      1 => 'second'
+    ];
+
+    foreach ($tokens as $unit => $text) {
+      if ($time < $unit) continue;
+      $numberOfUnits = floor($time / $unit);
+      return $numberOfUnits . ' ' . $text . (($numberOfUnits > 1) ? 's' : '');
+    }
+  }
+}
