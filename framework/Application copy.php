4ed6a8ba33eca0d35b49fe9a2e73777a73a717fc
MD. ABBAS UDDIN <76895700+mdabbas-cse@users.noreply.github.com>
2023-11-27 23:45:38 +0600

    Create Application copy.php

---
+++ framework/Application copy.php
@@ -0,0 +1,40 @@
+<?php
+
+// namespace LaraCore\Framework;
+
+class Application
+{
+  public function __construct()
+  {
+    $this->_set_reporting();
+    $this->_unregister_globals();
+  }
+
+  private function _set_reporting()
+  {
+    $debug = Configuration::get('app')['debug'];
+    if ($debug) {
+      error_reporting(E_ALL);
+      ini_set('display_errors', 0);
+      ini_set('log_errors', 1);
+      ini_set('error_log', ROOT . DS . 'resources' . DS . 'logs' . DS . 'error.log');
+    } else {
+      error_reporting(E_ALL);
+      ini_set('display_errors', 1);
+    }
+  }
+
+  private function _unregister_globals()
+  {
+    if (ini_get('register_globals')) {
+      $globalsAry = ['_SESSION', '_COOKIE', '_GET', '_POST', '_REQUEST', '_SERVER', '_ENV', '_FILES'];
+      foreach ($globalsAry as $g) {
+        foreach ($GLOBALS[$g] as $k => $v) {
+          if ($GLOBALS[$k] === $v) {
+            unset($GLOBALS[$k]);
+          }
+        }
+      }
+    }
+  }
+}
