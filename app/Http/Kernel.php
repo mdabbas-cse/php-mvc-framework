3e39294ad7c1f2ef0513a306fd397b9b154ac6be
MD. ABBAS UDDIN <76895700+mdabbas-cse@users.noreply.github.com>
2023-12-05 01:23:43 +0600

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,5 +9,6 @@ class Kernel
     'auth' => \LaraCore\App\Http\Middlewares\AuthMiddleware::class,
     'guest' => \LaraCore\App\Http\Middlewares\GuestMiddleware::class,
     'admin' => \LaraCore\App\Http\Middlewares\AdminMiddleware::class,
+    'authApi' => \LaraCore\App\Http\Middlewares\AuthApiMiddleware::class,
   ];
 }
