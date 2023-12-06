f1353ef7b9d38d4185ff7c9d5061f25e95f81614
MD. ABBAS UDDIN <76895700+mdabbas-cse@users.noreply.github.com>
2023-12-06 19:20:05 +0600

    change: api key name

--- framework/Console/ApiKey.php
+++ framework/Console/ApiKey.php
@@ -20,7 +20,7 @@ class ApiKey {
 
     $filePath = ROOT.DS.'.env';
     $contents = file_get_contents($filePath);
-    $key = 'API_KEY';
+    $key = 'API_TOKEN_KEY';
 
     // Find and replace the specified key with the new value
     $contents = preg_replace("/$key=.*/", "$key=$newKey", $contents);
