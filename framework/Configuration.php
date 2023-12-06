8dc4ae223246d822fdc59bcee90e956aa2133c30
MD. ABBAS UDDIN <76895700+mdabbas-cse@users.noreply.github.com>
2023-12-06 18:43:48 +0600

    formated

--- framework/Configuration.php
+++ framework/Configuration.php
@@ -9,20 +9,18 @@ use Exception;
  * @package LaraCore\Framework
  */
 
-class Configuration
-{
+class Configuration {
   /**
    * @param $key
    * @return array
    */
-  public static function get($key = null)
-  {
-    $configFilePath = ROOT . DS . 'config' . DS . 'Config.php';
-    if (!file_exists($configFilePath)) {
+  public static function get($key = null) {
+    $configFilePath = ROOT.DS.'config'.DS.'Config.php';
+    if(!file_exists($configFilePath)) {
       throw new Exception("Config file not found at {$configFilePath}");
     }
     $config = require($configFilePath);
-    if ($key && array_key_exists($key, $config)) {
+    if($key && array_key_exists($key, $config)) {
       return $config[$key];
     }
     die("Config key \"{$key}\" not found");
